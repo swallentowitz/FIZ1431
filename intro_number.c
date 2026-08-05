d9349aea52409f57d325ef0b9abad8172cfac293
Sascha Wallentowitz <166063323+swallentowitz@users.noreply.github.com>
2026-08-05 11:27:16 -0400

    updated

--- intro_number.c
+++ intro_number.c
@@ -1,4 +1,10 @@
-/* intro_number.c */
+/* intro_number.c
+   
+   Copyright (c) 2026 Sascha Wallentowitz
+   
+   Permission is granted under the MIT License.
+   See the LICENSE file for details.
+*/
 
 #include <stdio.h>
 
@@ -7,3 +13,5 @@ int main (void)
   printf("1.2 = %21.20f\n", 1.2);
   return 0;
 }
+
+
